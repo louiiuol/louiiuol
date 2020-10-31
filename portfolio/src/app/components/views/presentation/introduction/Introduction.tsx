742455207e7dbced8c28baa21ed7b094aadfd5a5
Louis Godlewski <louis.godlewski@gmail.com>
2020-10-31 22:41:53 +0100

    fix introduction typos

--- portfolio/src/app/components/views/presentation/introduction/Introduction.tsx
+++ portfolio/src/app/components/views/presentation/introduction/Introduction.tsx
@@ -18,7 +18,7 @@ const Introduction = () => {
                 </div>
                 <div className="is-white spaced-lg halfwidth-md no-overflow">
                     <p className='animate__animated animate__flipInX animate__delay-1s legend spaced-xs'>
-                        Passionné d'UX design et de structure de données, je suis un développeur web capable de contribuer à l'ensembles des étapes de construction d'un projet web.
+                        Passionné d'UX design et de structure de données, je suis un développeur web capable de contribuer à l'ensemble des étapes de construction d'un projet web.
                     </p>
                     <ul className='list circle bolded'>
                         <li className='animate__animated animate__fadeInLeft animate__delay-2s'>Création de composants graphiques comportant une logique d'affichage dédiée et réutilisable.</li>
@@ -26,7 +26,7 @@ const Introduction = () => {
                         <li className='animate__animated animate__fadeInLeft animate__delay-2s'>Vérification du code via des tests d'intégration ou unitaire.</li>
                         <li className='animate__animated animate__fadeInLeft animate__delay-2s'>Déploiement en continue des fonctionnalités via Github, Jenkins, AWS</li>
                     </ul>
-                    <p className='animate__animated animate__flipInX animate__delay-3s spaced-xs' >Je suis actuellement à la recherche d’un CDI en tant que développeur full stack Javascript. Si mon profi vous intéresse, n'hésitez pas à me contacter !</p>
+                    <p className='animate__animated animate__flipInX animate__delay-3s spaced-xs' >Je suis actuellement à la recherche d’un CDI en tant que développeur full stack Javascript. Si mon profil vous intéresse, n'hésitez pas à me contacter !</p>
                 </div>
             </section>
         </section>
