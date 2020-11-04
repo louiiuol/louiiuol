342ca15e50211b6d54aaf759f854d516763bd3f0
Louis Godlewski <louis.godlewski@gmail.com>
2020-11-04 16:12:50 +0100

    Adding gallery and improve Svg component

--- portfolio/src/app/components/shared/Svg.tsx
+++ portfolio/src/app/components/shared/Svg.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { ReactSVG } from 'react-svg';
 
-export const Svg = (props: {src: string, name: string, styles?: string, size?: string}) => {
+export const Svg = (props: {src: string, name: string, styles?: string, size?: string, onClick?: any}) => {
 
-    const IconWrapper = (icon: any) => (<ReactSVG className={`icon-svg ${icon.size} ${icon?.extra ? icon.extra : ''}`} src={icon.file} />);
+    const IconWrapper = (icon: any) => (<ReactSVG className={`icon-svg ${icon.size} ${icon?.extra ? icon.extra : ''}`} src={icon.file} onClick={icon.onClick} />);
     const ImportedIconRef = useRef(null);
     const [loading, setLoading] = useState(false);
         useEffect(() => {
@@ -22,6 +22,6 @@ export const Svg = (props: {src: string, name: string, styles?: string, size?: s
         importIcon();
         }, [props.name, props.src]);
         const { current: ImportedIcon } = ImportedIconRef;
-        return !loading && ImportedIconRef.current ? <IconWrapper size={props.size} file={ImportedIcon} extra={props.styles} />: null;
+        return !loading && ImportedIconRef.current ? <IconWrapper size={props.size} file={ImportedIcon} extra={props.styles} onClick={props.onClick} /> : null;
 
 };
--- portfolio/src/app/components/views/works/Graphism.tsx
+++ portfolio/src/app/components/views/works/Graphism.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import { Img } from '../../shared';
+import React, { useState } from 'react'
+import { Img, Svg } from '../../shared'
 
-import works from '../../../../assets/json/works/graphism.json';
+import works from '../../../../assets/json/works/graphism.json'
 
-export const Graphism = () =>
-    (<section id='graphism' className='container evenly'>
-        {works.map(graphic =>
-            <article key={graphic.id} className="graphic shadowed">
+export const Graphism = () => {
+
+    const [index, setIndex] = useState(0);
+    const [selected, setSelected] = useState({});
+    const [isSelected, setIsSelected] = useState(false);
+
+    const select = (currentIndex: number) => {
+        const workSize = works.length -1
+        if (currentIndex === 0) {
+            currentIndex = workSize;
+        } else if (currentIndex === workSize) {
+            currentIndex = 0;
+        }
+        setIndex(currentIndex);
+        setSelected(works[currentIndex]);
+        if (!isSelected) { setIsSelected(true) }
+        console.log(selected)
+    }
+
+    const hide = () => setIsSelected(false)
+
+    return (<section id='graphism' className='container evenly'>
+        {works.map((graphic, current) =>
+            <article key={graphic.id} className="graphic shadowed" onClick={() => select(current)}>
                 <Img src='works/graphism' name={graphic.src} alt={graphic.name}/>
                 <h2>{graphic.name}</h2>
                 <p>{graphic.description}</p>
             </article>
         )}
+        {isSelected && (<section id="gallery-fullscreen">
+            <ImgExpanded selected={selected} />
+            <nav>
+                <Svg src='ui' name='close' size='xs' styles='close is-white' onClick={() => hide()} />
+                <Svg src='ui' name='left' size='sm' styles='nav prev is-white' onClick={() => select(index - 1)} />
+                <Svg src='ui' name='left' size='sm' styles='nav next is-white' onClick={() => select(index + 1)}/>
+            </nav>
+        </section> )}
+
+    </section>)
+}
+
+export const ImgExpanded = (props: {selected: any}) =>
+    (<section className="selected">
+        <header><h3>{props.selected.name}</h3></header>
+        <Img src='works/graphism' name={props.selected.src} alt={props.selected.name} />
+        <p className="description">{props.selected?.description}</p>
     </section>)
