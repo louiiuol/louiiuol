1c2b3809fb6545b75e5a74cd286ae846f70192c8
Louis Godlewski <louis.godlewski@gmail.com>
2020-11-09 22:22:37 +0100

    Fixing constrcution frames

--- portfolio/src/app/components/views/works/Apps.tsx
+++ portfolio/src/app/components/views/works/Apps.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { QuarterContent } from '../../shared';
 
-import apps from '../../../../assets/json/works/apps.json';
-
 export const Apps = () => {
 
     return (
-        <QuarterContent id='apps'>
-            <section id="apps-gallery">
-                {apps.map((app, index) =>
-                    <article key={index}>
-                        {app}
-                    </article>)}
-            </section>
+        <QuarterContent id='algos'>
+            <div className='construction shadowed spaced-lg'>
+                <h1>Coming soon ..</h1>
+                <p>Cette section est en cours de production. Vous pouvez cependant retrouvez l'ensemble de mes travaux sur <a target='blank' href='https://github.com/louiiuol'>Github</a> et <a target='blank' href='https://codepen.io/louiiuol'>codepen</a> </p>
+            </div>
         </QuarterContent>)
 
 }
